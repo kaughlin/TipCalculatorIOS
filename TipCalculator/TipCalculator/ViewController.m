1261baab7b439c32776604149ac5ddbe8823cd8b
Kaughlin Caver <kaughlincaver@github.com>
2018-09-27 17:08:50 -0400

    tip calculator is now fully functional but no UI updates

--- TipCalculator/TipCalculator/ViewController.m
+++ TipCalculator/TipCalculator/ViewController.m
@@ -13,6 +13,7 @@
 @property (weak, nonatomic) IBOutlet UITextField *billField;
 @property (weak, nonatomic) IBOutlet UILabel *tipLabel;
 @property (weak, nonatomic) IBOutlet UILabel *totalLabel;
+    @property (weak, nonatomic) IBOutlet UISegmentedControl *tipControl;
 
 @end
 
@@ -33,9 +34,17 @@
     [self.view endEditing:YES];
 }
 - (IBAction)onEdit:(id)sender {
-    NSLog(@"on Edit");
+    
     double bill = [self.billField.text doubleValue];
-    double tip = 0.2 * bill;
+    
+    NSArray *percentages = @[@(0.15), @(.2), @(.22)];
+    
+    //in swift
+    //double tipPercentage = percentages[self.tipControl.selectedSegmentIndex].doubleValue();
+    double tipPercentage = [percentages[self.tipControl.selectedSegmentIndex] doubleValue];
+    
+    
+    double tip = tipPercentage * bill;
     double total = bill + tip;
     self.tipLabel.text = [NSString stringWithFormat:@"$%.2f", tip];
     self.totalLabel.text = [NSString stringWithFormat:@"$%.2f", total];
